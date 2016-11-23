ac20defaca671f0919b4098948af0924b6b82dbc
sylcastaing <castaing.sylvain@gmail.com>
2016-11-23 19:12:30 +0100

    Configure gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,12 +10,13 @@ var plugins = gulpLoadPlugins();
 const paths = {
   src: {
     folder: './src',
-    js: './src/js/*.js',
+    js: './src/**/*.js',
     html: './src/index.html'
   },
   build: {
     folder: './build',
-    js: './build/snake.js',
+    js: './build/js/',
+    jsName: 'snake.js',
     html: './build/index.html'
   },
   dist: {
@@ -29,7 +30,39 @@ let jsLint = lazypipe()
 
 gulp.task('default', ['dev']);
 
-gulp.task('dev', ['watch'], () => {
+gulp.task('clean', () => {
+  return gulp.src(paths.build.folder, {
+    read: false
+  })
+  .pipe(plugins.clean({
+    force: true
+  }));
+});
+
+gulp.task('build', ['clean'], () => {
+  gulp.src(paths.src.js)
+    .pipe(jsLint())
+    .pipe(plugins.concat(paths.build.jsName))
+    .pipe(gulp.dest(paths.build.js));
+
+  gulp.src(paths.src.html)
+    .pipe(plugins.htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest(paths.build.folder));
+});
+
+gulp.task('inject:js', () => {
+  var jsFile = gulp.src(paths.build.js + paths.build.jsName, {
+    read: false
+  });
+
+  return gulp.src(paths.build.html)
+    .pipe(plugins.inject(jsFile))
+    .pipe(gulp.dest(paths.build.html));
+});
+
+gulp.task('dev', ['build', 'watch'], () => {
   gulp.src(paths.build.folder)
     .pipe(server({
       livereload: true,
@@ -37,11 +70,18 @@ gulp.task('dev', ['watch'], () => {
     }));
 });
 
-gulp.task('watch', () => {
-  plugins.watch(paths.src.js)
-    .pipe(plugins.plumber())
-    .pipe(jsLint());
+gulp.task('watch', ['watch:js', 'watch:html']);
 
+gulp.task('watch:js', () => {
+  plugins.watch(paths.src.js, () => {
+    gulp.src(paths.src.js)
+      .pipe(jsLint())
+      .pipe(plugins.concat(paths.build.jsName))
+      .pipe(gulp.dest(paths.build.js));
+  })
+});
+
+gulp.task('watch:html', () => {
   plugins.watch(paths.src.html)
     .pipe(plugins.htmlmin({
       collapseWhitespace: true
---
+++ src/js/test2.js
@@ -0,0 +1,1 @@
+var bonjour = 1;
